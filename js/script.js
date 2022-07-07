925a8730532d339034401f0c77878a7ae2f912f7
Irene Martínez <98042321+Reniry@users.noreply.github.com>
2022-07-07 12:37:48 +0200

    Scroll up button and ordering Sass

--- js/script.js
+++ js/script.js
@@ -4,37 +4,29 @@ AOS.init();
 // Nav background
 var nav = document.querySelector('nav');
 
-      window.addEventListener('scroll', function () {
-        if (window.pageYOffset > 200) {
-          nav.classList.add('bg-info', 'shadow');
-        } else {
-          nav.classList.remove('bg-info', 'shadow');
-        }
-      });
+  window.addEventListener('scroll', function () {
+    if (window.pageYOffset > 200) {
+      nav.classList.add('bg-info', 'shadow');
+    } else {
+      nav.classList.remove('bg-info', 'shadow');
+    }
+  });
 
 // Boton scroll up
-document.getElementById('button-up').addEventListener('click', scrollUp)
+$(document).ready(function () {
+  $('.button-up').hide();
 
-function scrollUp(){
+  $(window).scroll(function () {
+    if ($(this).scrollTop () > 300 ) {
+      $('.button-up').fadeIn('750');
+    } else {
+      $('.button-up').fadeOut('750');
+    }
+  });
 
-  var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
-
-  if(currentScroll > 0){
-    window.requestAnimationFrame(scrollUp);
-    window.scrollTo (0, currentScroll - (currentScroll / 3));
-  }
-} 
-
-buttonUp = document.getElementById('button-up');
-
-window.onscroll = function () {
-
-  var scroll = document.documentElement.scrollTop;
-
-  if (scroll > 500) {
-    buttonUp.style.transform = 'scale(1)';
-  } else if(scroll < 500) {
-    buttonUp.style.transform = 'scale(0)';
-
-  }
-}
+  $('.button-up').click(function () {
+    $('body, html').animate({
+      scrollTop: 0
+    }, 500);
+  });
+});
